ba0d3a639209014635dc8b5d45929338190f381c
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-07-31 09:34:58 -0700

    Finished MVP.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Card from "./components/Card";
 import "./App.css";
 
 function App() {
+
   const [data, setData] = useState({});
-  // useEffect(() => {
-  //   axios.get("https://api.nasa.gov/planetary/apod?api_key=0KOwHwj2ahRDZ7JGNigjeHi9a9z0gdJJT4Vk4DmO")
-  //   .then(response => {
-  //     console.log(response)
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=0KOwHwj2ahRDZ7JGNigjeHi9a9z0gdJJT4Vk4DmO")
+      .then(response => {
+        console.log(response)
+        setData(response.data)
+      })
+    .catch(error => console.log('There is an error: ', error))
 
-  //   })
-  //   .catch(error => console.log('There is an error: ', error))
+  }, [])
 
-  // }, [])
+  console.log(data)
   
   return (
     <div className="App">
@@ -23,7 +27,13 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
-      <Card />
+      <Card 
+        copyright={data.copyright}
+        title={data.title}
+        date={data.date}
+        img={data.url}
+        explanation={data.explanation}
+      /> 
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  if(!props.img) return <h3>Loading.....</h3>
+  
   return (
     <div>
-      
+      <h1>{props.copyright}</h1>
+      <h3>{props.title}</h3>
+      <h3>{props.date}</h3>
+      <img src={props.img}></img>
+      <p>{props.explanation}</p>
     </div>
   )
 
 }
+
+//this comp
