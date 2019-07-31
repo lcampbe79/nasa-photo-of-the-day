56a5f002f65b750824978e3d53f977c4c4edf092
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-07-31 07:58:40 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./components/Card";
+
+
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  // useEffect(() => {
+  //   axios.get("https://api.nasa.gov/planetary/apod?api_key=0KOwHwj2ahRDZ7JGNigjeHi9a9z0gdJJT4Vk4DmO")
+  //   .then(response => {
+  //     console.log(response)
+
+  //   })
+  //   .catch(error => console.log('There is an error: ', error))
+
+  // }, [])
+  
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Card />
     </div>
   );
 }
